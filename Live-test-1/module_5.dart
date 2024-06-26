38bd56e0f460955028b4de601487b1dab3575d92
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-06-26 18:49:54 +0600

    done

---
+++ Live-test-1/module_5.dart
@@ -0,0 +1,20 @@
+class Media {
+  void play() {
+    print("Playing media...");
+  }
+}
+
+class Song extends Media {
+  String artist = "Arijit Singh";
+  @override
+  void play() {
+    print("Playing song by $artist...");
+  }
+}
+
+void main() {
+  Media obj1 = Media();
+  Song obj2 = Song();
+  obj1.play();
+  obj2.play();
+}
