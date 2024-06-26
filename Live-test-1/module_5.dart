eb3d0f5663298c87e215686536e33adda056d3bf
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-06-26 18:51:01 +0600

    added some comments

--- Live-test-1/module_5.dart
+++ Live-test-1/module_5.dart
@@ -1,20 +1,22 @@
 class Media {
+  //Base Class
   void play() {
     print("Playing media...");
   }
 }
 
 class Song extends Media {
-  String artist = "Arijit Singh";
+  //Derived Class
+  String artist = "Arijit Singh"; //Artist String
   @override
   void play() {
-    print("Playing song by $artist...");
+    print("Playing song by $artist..."); //Overriding
   }
 }
 
 void main() {
-  Media obj1 = Media();
-  Song obj2 = Song();
+  Media obj1 = Media(); //object of Media class
+  Song obj2 = Song(); //Object of Song Class
   obj1.play();
   obj2.play();
 }
