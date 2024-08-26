1771f6cfcf310a807db725f9e865b353a447ae08
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-08-27 00:52:51 +0600

    screens created

---
+++ Assignment-3/assignment3/lib/screen/desktop_view.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+Widget Desktop_View()
+{
+  return Scaffold();
+}
---
+++ Assignment-3/assignment3/lib/screen/tablet_view.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+Widget Tablet_View()
+{
+  return Scaffold();
+}
