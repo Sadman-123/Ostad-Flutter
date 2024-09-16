427959aed3741827bd21c30416315dbd2f542eb6
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-09-16 07:54:36 +0600

    stateful class convert

--- Assignment-5/assignment5/lib/screens/home.dart
+++ Assignment-5/assignment5/lib/screens/home.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-class Home extends StatelessWidget
+class Home extends StatefulWidget
 {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
    return Scaffold();
