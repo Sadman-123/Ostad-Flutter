77e6cde363c4c7b4388d9c7d1a39965e25b7d953
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-11-05 01:05:45 +0600

    task home fix

--- Assignment-6/assignment6/lib/task_screens/task_home.dart
+++ Assignment-6/assignment6/lib/task_screens/task_home.dart
@@ -1,44 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:task_manager/task_screens/task_cancelledtask.dart';
 import 'package:task_manager/task_screens/task_completedtask.dart';
 import 'package:task_manager/task_screens/task_newtask.dart';
 import 'package:task_manager/task_screens/task_progresstask.dart';
 import '../components/myappbar.dart';
-import '../controller/task_controller.dart';
 class TaskHome extends StatefulWidget {
   @override
   State<TaskHome> createState() => _TaskHomeState();
 }
-
 class _TaskHomeState extends State<TaskHome> {
-  TaskController taskController = Get.put(TaskController());
-
+  int ind=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Myappbar(autobar: false),
-      body: Obx(() => IndexedStack(
-        index: taskController.selectedIndex.value,
+      body: IndexedStack(
+        index: ind,
         children: [
           TaskNewtask(),
           TaskCompletedtask(),
           TaskCancelledtask(),
           TaskProgresstask(),
         ],
-      )),
-      bottomNavigationBar: Obx(() => NavigationBar(
+      ),
+      bottomNavigationBar: NavigationBar(
         onDestinationSelected: (value) {
-          taskController.changeIndex(value);
+          setState(() {
+            ind=value;
+          });
         },
-        selectedIndex: taskController.selectedIndex.value,
+        selectedIndex: ind,
         destinations: [
           NavigationDestination(icon: Icon(Icons.new_label_rounded), label: "New Task"),
           NavigationDestination(icon: Icon(Icons.check), label: "Completed"),
           NavigationDestination(icon: Icon(Icons.cancel), label: "Cancelled"),
           NavigationDestination(icon: Icon(Icons.timelapse_outlined), label: "Progress"),
         ],
-      )),
+      ),
     );
   }
 }
