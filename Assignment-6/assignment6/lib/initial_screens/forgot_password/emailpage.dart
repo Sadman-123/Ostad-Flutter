a607e4a101b8750351d9661d0d54a14ca66a36ff
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-11-04 23:08:18 +0600

    email fix

--- Assignment-6/assignment6/lib/initial_screens/forgot_password/emailpage.dart
+++ Assignment-6/assignment6/lib/initial_screens/forgot_password/emailpage.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:task_manager/components/have_act_msg.dart';
-import 'package:task_manager/controller/user_controller.dart';
 import 'package:task_manager/style/style.dart';
+import 'package:http/http.dart' as http;
 import '../../components/mybutton.dart';
-class Emailpage extends StatelessWidget{
-  UserController user=Get.find();
+class Emailpage extends StatefulWidget{
+  @override
+  State<Emailpage> createState() => _EmailpageState();
+}
+class _EmailpageState extends State<Emailpage> {
+  TextEditingController recovery_email=TextEditingController();
+  Future<void> send_otp() async {
+    try {
+      var url = Uri.parse("http://35.73.30.144:2005/api/v1/RecoverVerifyEmail/${recovery_email.text.toString()}");
+      print("Sending OTP to: ${recovery_email.text}");
+      var res = await http.get(url, headers: {'Content-Type': 'application/json'});
+      print("Response status: ${res.statusCode}");
+      print("Response body: ${res.body}");
+      if (res.statusCode == 200) {
+        Fluttertoast.showToast(
+          msg: "OTP Sent Successfully",
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Color(0xFF20be73),
+          textColor: Colors.white,
+        );
+        Navigator.pushNamed(context, '/OtpVerify');
+      } else {
+        Fluttertoast.showToast(
+          msg: "Something Went Wrong: ${res.body}",
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.black,
+          textColor: Colors.white,
+        );
+      }
+    } catch (e) {
+      print("Error: $e");
+      Fluttertoast.showToast(
+        msg: "An error occurred: $e",
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.black,
+        textColor: Colors.white,
+      );
+    }
+  }
   @override
   Widget build(BuildContext context) {
     var mdw=MediaQuery.sizeOf(context).width;
@@ -40,14 +77,14 @@ class Emailpage extends StatelessWidget{
 
                           SizedBox(height: mdh*0.035,),
                           TextFormField(
-                            controller: user.recovery_email,
+                            controller: recovery_email,
                             decoration: InputDecoration(
                                 hintText: "Email"
                             ),
                           ),
                           SizedBox(height: mdh*0.012,),
                           SizedBox(height: mdh*0.012,),
-                          Mybutton(onpressed: (){user.send_otp();},something: Icon(Icons.navigate_next),)
+                          Mybutton(onpressed: (){send_otp();},something: Icon(Icons.navigate_next),)
                         ],
                       ),
                     ),
